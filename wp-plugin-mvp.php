7ec05542cb66b42b909da8c31cc00b1c531383dd
Nabil Kadimi <nabil@kadimi.com>
2017-08-22 01:40:06 +0100

    Create minimal plugin

---
+++ wp-plugin-mvp.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Plugin MVP
+Plugin URI: https://kadimi.com
+Description: Plugin description.
+Version: 1.0.0
+Author: Nabil Kadimi.
+Author URI: https://kadimi.com
+License: GPL2
+*/
